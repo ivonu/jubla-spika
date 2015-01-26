c0a8f6d5fc62350bcbf06bc5870385231248ae14
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2015-01-26 18:42:36 +0100

    included juba-themed bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users
   root 'users#index'
+  get 'bootstrap' => 'application#bootstrap'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
