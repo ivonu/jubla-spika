5ef7f9d12550162e548d2840985f7452c2d4c603
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2015-03-02 16:48:49 +0100

    add confirmable to devise

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'turbolinks'
 gem 'devise'
 gem 'pundit'
 
+gem 'figaro'
+
 group :development, :test do
   gem 'sqlite3'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   enum role: { user: 0, moderator: 1, admin: 2 }
   after_initialize :set_default_role, if: :new_record?
