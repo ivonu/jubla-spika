02e610aa76306990ffef1097ae896e85481be2ed
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2015-03-25 15:55:51 +0100

    save thumbnails of images

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,15 +1,22 @@
 class Attachment < ActiveRecord::Base
   belongs_to :entry
 
-  has_attached_file :file
-  validates_attachment_content_type :file, content_type: ['application/pdf',
-                                                          'image/gif',
-                                                          'image/jpeg',
-                                                          'image/png',
-                                                          'text/plain',
-                                                          'text/rtf',
-                                                          'application/vnd.ms-excel',
-                                                          'application/msword',
-                                                          'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-                                                          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+  has_attached_file :file,
+    styles: lambda{ |a|
+      %w(image/jpeg image/png image/jpg image/gif).include?(a.content_type) ?
+          { :thumb=> "100x100#", } :
+          { }
+    }
+
+  validates_attachment_content_type :file, content_type: %w(application/pdf'
+                                                            image/gif
+                                                            image/jpg
+                                                            image/jpeg
+                                                            image/png
+                                                            text/plain
+                                                            text/rtf
+                                                            application/vnd.ms-excel
+                                                            application/msword
+                                                            application/vnd.openxmlformats-officedocument.wordprocessingml.document
+                                                            application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)
 end
