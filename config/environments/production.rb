d1aab1b392c6fff81325d9de9773dc5e6c60a17d
Silvan Melchior <silvanmelchior@gmail.com>
2015-08-27 11:55:40 -0700

    confirmation new server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'spielkatapult.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'spika.jubla.ch' }
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'nerthus.metanet.ch',
