20911b7d3a0873c0acd63981663d484382e2939d
Silvan Melchior <silvanmelchior@gmail.com>
2015-09-01 16:56:51 +0200

    empty names not allowed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   has_many :programs
   has_many :entries
 
+  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
